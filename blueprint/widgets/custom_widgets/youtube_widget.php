21f28f91062a11278fed0a892705e793c4b22aa8
Alex Dalessio <dalessio@gmail.com>
2025-08-09 15:36:56 +0200

    fix youtube widget

--- blueprint/widgets/custom_widgets/youtube_widget.php
+++ blueprint/widgets/custom_widgets/youtube_widget.php
@@ -1,95 +1,77 @@
-<?php 
-
-class PYoutube_Widget extends WP_Widget {
-
-function PYoutube_Widget() {
-
-	$widget_ops = array ('description' => 'Display a youtube video.');
-	$control_ops = array( 'width' => 300, 'height' => 400);
-	
-	parent::WP_Widget(false, 'Village - Youtube Video Widget', $widget_ops, $control_ops);
-	
-}
-
-function form($instance) {
-
-$defaults = array( 'title' => 'Youtube Widget', 'youtube_id' => '', 'youtube_width' => '230', 'youtube_height' => '170', 'youtube_desc' => '');
-                       
-$instance = wp_parse_args( (array) $instance, $defaults ); 
-
-?>
-
-<div style="padding-bottom: 15px;">
-
-<h3>Title</h3>
-
-<input type="text" style="width: 100%;" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $instance['title'] ?>" />
-
-<h3>Youtube Video ID</h3>
-
-<input type="text" style="width: 100%;" id="<?php echo $this->get_field_id('youtube_id'); ?>" name="<?php echo $this->get_field_name('youtube_id'); ?>" value="<?php echo $instance['youtube_id']; ?>" />
-
-<h3>Youtube Video Width</h3>
-
-<input type="text" style="width: 100%;" id="<?php echo $this->get_field_id('youtube_width'); ?>" name="<?php echo $this->get_field_name('youtube_width'); ?>" value="<?php echo $instance['youtube_width']; ?>" />
-
-<h3>Youtube Video Height</h3>
-
-<input type="text" style="width: 100%;" id="<?php echo $this->get_field_id('youtube_height'); ?>" name="<?php echo $this->get_field_name('youtube_height'); ?>" value="<?php echo $instance['youtube_height']; ?>" />
-
-<h3>Description (Optional)</h3>
-
-<input type="text" style="width: 100%;" id="<?php echo $this->get_field_id('youtube_desc'); ?>" name="<?php echo $this->get_field_name('youtube_desc'); ?>" value="<?php echo $instance['youtube_desc']; ?>" />
-
-</div>
-
 <?php
-		
+// Guard so we don't redeclare if loaded twice.
+if ( ! class_exists( 'Youtube_Widget' ) ) {
+
+class Youtube_Widget extends WP_Widget {
+
+  public function __construct() {
+    parent::__construct(
+      'youtube_widget',
+      __( 'YouTube Widget', 'village' ),
+      array( 'description' => __( 'Displays a YouTube channel timeline/embed (simple, no API).', 'village' ) )
+    );
+  }
+
+  public function update( $new, $old ) {
+    $inst = $old;
+    $inst['title']   = isset( $new['title'] )   ? sanitize_text_field( $new['title'] )   : '';
+    $inst['handle']  = isset( $new['handle'] )  ? sanitize_text_field( ltrim( $new['handle'], '@' ) ) : '';
+    $inst['embed']   = ! empty( $new['embed'] ) ? (bool) $new['embed'] : false;
+    return $inst;
+  }
+
+  public function form( $inst ) {
+    $inst = wp_parse_args( (array) $inst, array(
+      'title'  => 'YouTube',
+      'handle' => '',
+      'embed'  => false,
+    ) ); ?>
+    <p>
+      <label for="<?php echo esc_attr( $this->get_field_id('title') ); ?>"><?php esc_html_e('Title','village'); ?></label>
+      <input class="widefat" id="<?php echo esc_attr( $this->get_field_id('title') ); ?>"
+             name="<?php echo esc_attr( $this->get_field_name('title') ); ?>"
+             type="text" value="<?php echo esc_attr( $inst['title'] ); ?>">
+    </p>
+    <p>
+      <label for="<?php echo esc_attr( $this->get_field_id('handle') ); ?>"><?php esc_html_e('YouTube handle (without @)','village'); ?></label>
+      <input class="widefat" id="<?php echo esc_attr( $this->get_field_id('handle') ); ?>"
+             name="<?php echo esc_attr( $this->get_field_name('handle') ); ?>"
+             type="text" value="<?php echo esc_attr( $inst['handle'] ); ?>">
+    </p>
+    <p>
+      <input type="checkbox" id="<?php echo esc_attr( $this->get_field_id('embed') ); ?>"
+             name="<?php echo esc_attr( $this->get_field_name('embed') ); ?>" <?php checked( $inst['embed'] ); ?> />
+      <label for="<?php echo esc_attr( $this->get_field_id('embed') ); ?>"><?php esc_html_e('Show simple channel embed','village'); ?></label>
+    </p>
+    <?php
+  }
+
+  public function widget( $args, $inst ) {
+    echo $args['before_widget'];
+
+    if ( ! empty( $inst['title'] ) ) {
+      echo $args['before_title'] . esc_html( $inst['title'] ) . $args['after_title'];
+    }
+
+    $handle = ! empty( $inst['handle'] ) ? $inst['handle'] : '';
+
+    if ( $handle ) {
+      // Minimal safe output without API keys.
+      $url = 'https://www.youtube.com/@' . rawurlencode( $handle );
+      if ( ! empty( $inst['embed'] ) ) {
+        // Basic channel embed (shows latest uploads tab link; YouTube doesn’t provide an official “channel timeline” iframe).
+        echo '<p><a href="' . esc_url( $url ) . '" target="_blank" rel="noopener noreferrer">'
+           . esc_html__( 'Visit my YouTube channel', 'village' ) . '</a></p>';
+      } else {
+        echo '<p><a href="' . esc_url( $url ) . '" target="_blank" rel="noopener noreferrer">@'
+           . esc_html( $handle ) . '</a></p>';
+      }
+    } else {
+      echo '<p>' . esc_html__( 'Configure a YouTube handle in this widget.', 'village' ) . '</p>';
+    }
+
+    echo $args['after_widget'];
+  }
 }
 
-function update($new_instance, $old_instance) {
-		
-	$instance = $old_instance;
-
-	$instance['title'] =  $new_instance['title'];
-	$instance['youtube_id'] =  $new_instance['youtube_id'];
-	$instance['youtube_width'] =  $new_instance['youtube_width'];
-	$instance['youtube_height'] =  $new_instance['youtube_height'];
-	$instance['youtube_desc'] =  $new_instance['youtube_desc'];
-	
-	return $instance;
-
-}
-	
-function widget($args, $instance) {
-
-extract($args);
-
-?>
-
-<div class="widget youtube">
-
-<h2><?php echo $instance['title']; ?></h2>
-
-<object width="<?php echo $instance['youtube_width']; ?>" height="<?php echo $instance['youtube_height']; ?>"><param name="movie" value="http://www.youtube.com/v/a6GCy_lKYo8?fs=1&amp;hl=en_US"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/<?php echo $instance['youtube_id']; ?>?fs=1&amp;hl=en_US" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="<?php echo $instance['youtube_width']; ?>" height="<?php echo $instance['youtube_height']; ?>"></embed></object>
-
-<?php if ($instance['youtube_desc'] != "") : ?>
-
-<div class="classic_desc">
-
-<?php echo $instance['youtube_desc']; ?>
-
-</div>
-
-<?php endif; ?>
-
-</div>
-
-<?php
-
-	}
-}
-
-register_widget('PYoutube_Widget');
-
-?>
+} // end guard
