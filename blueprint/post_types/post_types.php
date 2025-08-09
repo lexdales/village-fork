8db3767bdda2f05ec27fc33a5db818b5ccbb7fc6
Alex Dalessio <dalessio@gmail.com>
2025-08-09 15:45:52 +0200

    fix meta_box

--- blueprint/post_types/post_types.php
+++ blueprint/post_types/post_types.php
@@ -1,83 +1,98 @@
-<?php 
-
+<?php
 #####################################################
 ##### BLUEPRINT - POST TYPES
 #####################################################
 
-add_action("init", "blueprint_register_post_types");
+add_action( 'init', 'blueprint_register_post_types' );
 
-function blueprint_register_post_types () {
+function blueprint_register_post_types() {
+    global $blueprint_post_type_array;
 
-global $blueprint_post_type_array;
+    if ( empty( $blueprint_post_type_array ) || empty( $blueprint_post_type_array[0] ) ) {
+        return;
+    }
 
-if ($blueprint_post_type_array[0]) :
+    foreach ( $blueprint_post_type_array as $post_type ) {
 
-foreach ($blueprint_post_type_array as $post_type_count => $post_type) :
+        $labels = array(
+            'name'               => $post_type['post_type_plural_label_name'],
+            'singular_name'      => $post_type['post_type_label_name'],
+            'add_new'            => _x( 'Add New', 'slider' ),
+            'add_new_item'       => 'Add A New ' . $post_type['post_type_label_name'],
+            'edit_item'          => 'Edit ' . $post_type['post_type_label_name'],
+            'new_item'           => 'New ' . $post_type['post_type_label_name'],
+            'view_item'          => 'View ' . $post_type['post_type_label_name'],
+            'search_items'       => 'Search ' . $post_type['post_type_plural_label_name'],
+            'not_found'          => 'No ' . strtolower( $post_type['post_type_plural_label_name'] ) . ' found',
+            'not_found_in_trash' => 'No ' . strtolower( $post_type['post_type_plural_label_name'] ) . ' found in Trash',
+            'parent_item_colon'  => '',
+            'menu_name'          => $post_type['post_type_plural_label_name'],
+        );
 
-$labels = array(
+        // Only set the meta box callback if it exists and is callable.
+        $register_meta_box_cb = null;
+        if ( ! empty( $post_type['meta_box_function'] ) && is_string( $post_type['meta_box_function'] ) && is_callable( $post_type['meta_box_function'] ) ) {
+            $register_meta_box_cb = $post_type['meta_box_function'];
+        }
 
-    'name' => __($post_type["post_type_plural_label_name"], 'post type general name'),
-    'singular_name' => __($post_type["post_type_label_name"], 'post type singular name'),
-    'add_new' => _x('Add New', 'slider'),
-    'add_new_item' => __('Add A New ' . $post_type["post_type_label_name"] . ''),
-    'edit_item' => __('Edit ' . $post_type["post_type_label_name"] . ''),
-    'new_item' => __('New ' . $post_type["post_type_label_name"] . ''),
-    'view_item' => __('View ' . $post_type["post_type_label_name"] . ''),
-    'search_items' => __('Search ' . $post_type["post_type_plural_label_name"] . ''),
-    'not_found' =>  __('No ' . strtolower($post_type["post_type_plural_label_name"]) . ' found'),
-    'not_found_in_trash' => __('No ' . strtolower($post_type["post_type_plural_label_name"]) . ' found in Trash'), 
-    'parent_item_colon' => '',
-    'menu_name' => '' . $post_type["post_type_plural_label_name"] . ''
+        $args = array(
+            'labels'              => $labels,
+            'public'              => true,
+            'show_ui'             => true,
+            'publicly_queryable'  => true,
+            'capability_type'     => 'post',
+            'supports'            => isset( $post_type['supports_array'] ) ? (array) $post_type['supports_array'] : array( 'title', 'editor' ),
+            'query_var'           => true,
+            'rewrite'             => true,
+            'menu_icon'           => isset( $post_type['post_type_icon'] ) ? $post_type['post_type_icon'] : null,
+        );
 
-);
+        if ( $register_meta_box_cb ) {
+            $args['register_meta_box_cb'] = $register_meta_box_cb;
+        }
 
-$args = array (
-'labels' => $labels,
-'public' => true,
-'show_ui' => true,
-'publicly_queryable' => true,
-'capability_type' => 'post',
-'supports' => $post_type["supports_array"],
-'register_meta_box_cb' => $post_type['meta_box_function'],
-'query_var' => true,
-'rewrite' => true,
-'menu_icon' => $post_type['post_type_icon']
-);
+        register_post_type( $post_type['post_type_name'], $args );
 
-register_post_type($post_type['post_type_name'], $args);
+        // Taxonomy (optional)
+        if ( ! empty( $post_type['use_taxonomy'] ) ) {
+            $g_labels = array(
+                'name'                       => $post_type['taxonomy_plural_label_name'],
+                'singular_name'              => $post_type['taxonomy_label_name'],
+                'search_items'               => 'Search ' . $post_type['taxonomy_plural_label_name'],
+                'popular_items'              => 'Popular ' . $post_type['taxonomy_plural_label_name'],
+                'all_items'                  => 'All ' . $post_type['taxonomy_plural_label_name'],
+                'parent_item'                => null,
+                'parent_item_colon'          => null,
+                'edit_item'                  => 'Edit ' . $post_type['taxonomy_label_name'],
+                'update_item'                => 'Update ' . $post_type['taxonomy_label_name'],
+                'add_new_item'               => 'Add New ' . $post_type['taxonomy_label_name'],
+                'new_item_name'              => 'New ' . $post_type['taxonomy_label_name'] . ' Name',
+                'separate_items_with_commas' => 'Separate ' . strtolower( $post_type['taxonomy_plural_label_name'] ) . ' with commas',
+                'add_or_remove_items'        => 'Add or remove ' . strtolower( $post_type['taxonomy_plural_label_name'] ),
+                'choose_from_most_used'      => 'Choose from the most used ' . strtolower( $post_type['taxonomy_plural_label_name'] ),
+                'menu_name'                  => $post_type['taxonomy_plural_label_name'],
+            );
 
-flush_rewrite_rules();
-
-$g_labels = array(
-    'name' => __( '' . $post_type["taxonomy_plural_label_name"] . '', 'taxonomy general name' ),
-    'singular_name' => __( '' . $post_type["taxonomy_label_name"] . '', 'taxonomy singular name' ),
-    'search_items' =>  __( 'Search ' . $post_type["taxonomy_plural_label_name"] . '' ),
-    'popular_items' => __( 'Popular ' . $post_type["taxonomy_plural_label_name"] . '' ),
-    'all_items' => __( 'All ' . $post_type["taxonomy_plural_label_name"] . '' ),
-    'parent_item' => null,
-    'parent_item_colon' => null,
-    'edit_item' => __( 'Edit ' . $post_type["taxonomy_label_name"] . '' ), 
-    'update_item' => __( 'Update ' . $post_type["taxonomy_label_name"] . '' ),
-    'add_new_item' => __( 'Add New ' . $post_type["taxonomy_label_name"] . '' ),
-    'new_item_name' => __( 'New ' . $post_type["taxonomy_label_name"] . ' Name' ),
-    'separate_items_with_commas' => __( 'Separate ' . strtolower($post_type["taxonomy_plural_label_name"]) . ' with commas' ),
-    'add_or_remove_items' => __( 'Add or remove ' . strtolower($post_type["taxonomy_plural_label_name"]) . '' ),
-    'choose_from_most_used' => __( 'Choose from the most used ' . strtolower($post_type["taxonomy_plural_label_name"]) . '' ),
-    'menu_name' => __( '' . $post_type["taxonomy_plural_label_name"] . '' ),
-);
-
-if ($post_type['use_taxonomy'] == true) {
-   
-register_taxonomy($post_type['taxonomy_name'], array($post_type['post_type_name']), array("hierarchical" => true, "label" => "Sliders", "singular_label" => "Slider Category", "labels" => $g_labels, 'query_var' => true, "rewrite" => true, "show_in_nav_menus" => true));
-
-flush_rewrite_rules();
-
+            register_taxonomy(
+                $post_type['taxonomy_name'],
+                array( $post_type['post_type_name'] ),
+                array(
+                    'hierarchical'       => true,
+                    'labels'             => $g_labels,
+                    'query_var'          => true,
+                    'rewrite'            => true,
+                    'show_in_nav_menus'  => true,
+                )
+            );
+        }
+    }
 }
 
-endforeach;
-
-endif;
-
-}
-
-?>
+/**
+ * Important: do NOT flush rewrite rules on every request.
+ * Flush once on theme switch/activation instead.
+ */
+add_action( 'after_switch_theme', function () {
+    blueprint_register_post_types();
+    flush_rewrite_rules();
+} );
